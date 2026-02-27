685603901d4a831d9ef0efa34155b2a5f846a2d7
Haikal018 <haikalkahfi010575@gmail.com>
2026-02-27 23:47:52 +0700

    Tugas_Stream

---
+++ JavaStream.java
@@ -0,0 +1,20 @@
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class JavaStream {
+    public static void main(String[] args) {
+        try {
+            OutputStream os = new FileOutputStream("D:/Coolyah/Kelas 2B Sem 4/Pemrograman Jaringan/praktek1.txt");
+            Scanner s = new Scanner(System.in);
+            System.out.println("Masukkan kalimat =");
+            String data = s.nextLine();
+            os.write(data.getBytes());
+            os.flush();
+            os.close();
+            System.out.println("Pengiriman Data Berhasil");
+        } catch (Exception e) {
+            System.err.println("Pengiriman Data Tidak Berhasil");
+        }
+    }
+}
